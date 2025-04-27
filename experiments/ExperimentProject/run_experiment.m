99256b910febf469c12a379eb11852b92d9aeba4
yangliu-op <yangliu.op@gmail.com>
2025-04-28 02:21:07 +1000

    Make sure AR2 is running correctly

--- experiments/ExperimentProject/run_experiment.m
+++ experiments/ExperimentProject/run_experiment.m
@@ -2,41 +2,40 @@
 % Ensure that you run `setup` before this. The options for each parameter that
 % are presented in this file are suggestions, but do not cover the whole range
 % of possible parameter combinations.
+% We use `inner_` to refer options at the AR3 subproblem inner solver level.
+% We use `inner_inner_` to refer options at the AR2 subproblem inner solver level.
+% MCMR extends MCM into any order.
 
 format short g;
 % setup;
 
+% AR2 or AR3
 p = {
      2
      % 3
     };
 
+% Choose initial sigma0
 update_sigma0 = {
-                 % 1e-8
-                 % 'LIPSCHITZ'
-                 % 'INVEXIFICATION'
+                 % 1e-8 % any positive real number
                  'TAYLOR'
                 };
 
+% Different test problems
 problems = {
-            % 'rosenbrock'
-            % 'chebysv_rosenbrock'
-            % 'quartic'
-            % 'square'
-            % 'd1_fun'
+            1 % MGH test, 1 to 35
+            % 'rosenbrock' % Multidimensional Rosenbrock
             % 'nonlinear_least_squares'
-            % 'ill_cond_bm'
-            % 'ill_cond_H'
-            % 'ill_cond_T'
-            % 'ill_cond_HT'
-            % 'separable_function'
-            % 'non_separable_function'
-            1 % MGH test
+            % 'ill_cond_bm' % Well-conditioned Regularized 3rd-order polynomials
+            % 'ill_cond_H' % Ill-conditioned Hessian ...
+            % 'ill_cond_T' % Ill-conditioned Tensor ...
+            % 'ill_cond_HT' % Ill-conditioned Hessian and Tensor ...
            };
 
 if isnumeric(problems{1})
     problem = string(problems{1});
 else
+    % Set dimensions for Non-MGH problems
     if strcmp(problems{1}, "d1_fun")
         problem = jsonencode(struct(name = problems{1}, dim = 1));
     else
@@ -44,82 +43,77 @@ else
     end
 end
 
+% Starting point
 x0_type = {
            'default'
-           % -1.205
            % 'randn'
            % 'rand'
            % 'uniform'
           };
 
+% Three update options for the main algorithm
 update_type = {
                % 'Simple'
-               % 'Interpolation_t'
                'Interpolation_m'
                % 'BGMS'
               };
 
+% Activate pre-rejection module
 update_use_prerejection = {
                            true
                            % false
                           };
 
-inner_solver = {
-                'AR2'
-                % 'fminunc_trust-region',
-                % 'fminunc_lbfgs',
-                % 'fminunc_dfp',
-                % 'fminunc_steepdesc',
-               };
-
+% Three update options for the inner solver AR2
 inner_update_type = {
                      'Simple'
-                     % 'Interpolation_t'
                      % 'Interpolation_m'
                      % 'BGMS'
                     };
 
-inner_inner_solver = {
-                      'MCMR'
-                      % 'trust-region'
-                     };
-
+% Termination rule of the inner solver of AR3
 inner_stop_rule = {
                    'First_Order'
                    % 'ARP_Theory'
                    % 'General_Norm'
-                   % 'Cartis_G'
-                   % 'Cartis_S'
                   };
 
-inner_inner_stop_rule = {
-                         'First_Order'
-                         % 'ARP_Theory'
-                         % 'Cartis_G'
-                         % 'Cartis_S'
-                        };
-
 % Run experiment for the given parameters
-params = struct( ...
-                p = p{1}, ...
-                problem = problem, ...
-                x0_type = x0_type{1}, ...
-                update_sigma0 = update_sigma0{1}, ...
-                update_type = update_type{1}, ...
-                update_use_prerejection = update_use_prerejection{1}, ...
-                stop_rule = 'First_Order', ...
-                stop_tolerance_g = 1e-8, ...
-                inner_solver = inner_solver{1}, ...
-                inner_update_type = inner_update_type{1}, ...
-                inner_stop_rule = inner_stop_rule{1}, ...
-                inner_stop_tolerance_g = 1e-9, ...
-                inner_inner_solver = inner_inner_solver{1}, ...
-                inner_inner_stop_rule = inner_inner_stop_rule{1}, ...
-                inner_inner_stop_tolerance_g = 1e-10);
+if p{1} == 3
+    params = struct( ...
+                    p = 3, ...
+                    problem = problem, ...
+                    x0_type = x0_type{1}, ...
+                    update_sigma0 = update_sigma0{1}, ...
+                    update_type = update_type{1}, ...
+                    update_use_prerejection = update_use_prerejection{1}, ...
+                    stop_rule = 'First_Order', ...
+                    stop_tolerance_g = 1e-8, ...
+                    inner_solver = 'AR2', ...
+                    inner_update_type = inner_update_type{1}, ...
+                    inner_stop_rule = inner_stop_rule{1}, ...
+                    inner_stop_tolerance_g = 1e-9, ...
+                    inner_inner_solver = 'MCMR', ...
+                    inner_inner_stop_rule = 'First_Order', ...
+                    inner_inner_stop_tolerance_g = 1e-10);
+else
+    params = struct( ...
+                    p = 2, ...
+                    problem = problem, ...
+                    x0_type = x0_type{1}, ...
+                    update_sigma0 = update_sigma0{1}, ...
+                    update_type = update_type{1}, ...
+                    stop_rule = 'First_Order', ...
+                    stop_tolerance_g = 1e-8, ...
+                    inner_solver = 'MCMR', ...
+                    inner_stop_rule = inner_stop_rule{1}, ...
+                    inner_stop_tolerance_g = 1e-9);
+    update_use_prerejection = {false};
+end
 table = struct2table(training(params));
 
 % Save the run history to its own file
-folder = append('records/', problem);
+folder = append('./records/', problem);
 [~, ~] = mkdir(folder);
 name = append(folder, '/', update_type{1}, '_', inner_stop_rule{1}, ...
               '_', x0_type{1}, '_', num2str(p{1}), '_sigma_', num2str(update_sigma0{1}), ...
