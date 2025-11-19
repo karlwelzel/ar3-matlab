c476654efa3869a5f6f21371877154c4263d93e8
Yang Liu <liuy@maths.ox.ac.uk>
2025-11-19 16:12:28 +0000

    Typo fixed

--- libraries/ar3/tensor_vec.m
+++ libraries/ar3/tensor_vec.m
@@ -2,6 +2,6 @@ function [tesorvec] = tensor_vec(tesor, vec)
     if isa(tesor, 'function_handle')
         tesorvec = tesor(vec);
     else
-        tesorvec = tensorprod(der3f, vec, 1);
+        tesorvec = tensorprod(tesor, vec, 1);
     end
 end
